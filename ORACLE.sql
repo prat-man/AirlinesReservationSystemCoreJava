e5e975dbcd51edf957cd2e7ce0f12313ce6e268f
Sajal Bain <virusdata@hotmail.com>
2018-09-21 15:10:11 +0530

    naya SQL File diya h

--- ORACLE.sql
+++ ORACLE.sql
@@ -16,11 +16,22 @@ CREATE TABLE airport
 	location varchar2(40), PRIMARY KEY(abbreviation)
 );
 
-create table flightinformation
+Flight Information Table:
+CREATE TABLE flightinformation
 (
 	flightno varchar2(5),airline varchar2(10),depcity varchar2(10),
 	arrcity varchar2(10),depdate date,arrdate date,deptime number(8),
 	arrtime number(8),firstseats number(6),firstseatfare number(8,2),
 	bussseats number(6),bussseatsfare number(6,2),
 	primary key(flightno)
+);
+
+Booking Information Table:
+CREATE TABLE bookinginformation 
+(
+	booking_id varchar2(5),cust_email varchar2(30),
+	no_of_passengers number(3),class_type varchar2(10),
+	total_fare number(6,2),seat_number number(3),
+	creditcard_info varchar2(16),src_city varchar2(10),
+	dest_city varchar2(10),primary key(booking_id)
 );
