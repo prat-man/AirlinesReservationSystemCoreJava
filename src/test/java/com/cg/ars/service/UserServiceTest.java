439c08b94b1709b02f407a9ab031c5323929d5ff
Pratanu Mandal <pratanu.mandal@gmail.com>
2018-09-25 15:59:06 +0530

    Fixed User Service

--- src/test/java/com/cg/ars/service/UserServiceTest.java
+++ src/test/java/com/cg/ars/service/UserServiceTest.java
@@ -17,6 +17,18 @@ public class UserServiceTest
 	@Test
 	public void testValidateMobileNo1() throws UserException
 	{
-		Assert.assertEquals(true, user.validateMobileNo("+91-8582964407"));
+		Assert.assertEquals(true, user.validateMobileNo("+91-562-8582964407"));
+	}
+	
+	@Test
+	public void testValidateMobileNo2() throws UserException
+	{
+		Assert.assertEquals(true, user.validateMobileNo("+91 85829-64407"));
+	}
+	
+	@Test
+	public void testValidateMobileNo3() throws UserException
+	{
+		Assert.assertEquals(true, user.validateMobileNo("+91 85829 64407"));
 	}
 }
