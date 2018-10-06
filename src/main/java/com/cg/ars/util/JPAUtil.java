ed248eda025451fd882a9efb7e10ff514f8a61c5
Pratanu Mandal <pratanu.mandal@gmail.com>
2018-10-06 20:10:52 +0530

    Trying to Fix Major Bugs

--- src/main/java/com/cg/ars/util/JPAUtil.java
+++ src/main/java/com/cg/ars/util/JPAUtil.java
@@ -1,5 +1,8 @@
 package com.cg.ars.util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -13,10 +16,20 @@ public class JPAUtil
 		factory = Persistence.createEntityManagerFactory("JPA-PU");
 	}
 	
-	public static EntityManager getEntityManager() {
-		if(entityManager == null || !entityManager.isOpen()) {
+	public static EntityManager getEntityManager()
+	{
+		if (entityManager == null || !entityManager.isOpen()) {
 			entityManager = factory.createEntityManager();
+			
+			Connection connection = entityManager.unwrap(Connection.class);
+			
+			try {
+				connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} 
 		}
+		
 		return entityManager;
 	}
 }
